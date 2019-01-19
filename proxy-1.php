4fee7c7f606e6468ceb04d109fac0d9f0027690e
Matej Bádal <matejbadal@gmail.com>
2019-01-19 22:15:24 +0100

    updated instructions for proxy

--- proxy-1.php
+++ proxy-1.php
@@ -7,9 +7,18 @@ use Delvesoft\ServiceLayer\ValueObject\Url;
 
 require 'vendor/autoload.php';
 
+/**
+ * Instrukcie:
+ * - V balicku 'Delvesoft\ServiceLayer' je implemebtovana jednoducha servisna vrstva, ktora moze stahovat velke objemy dat
+ * - Zabezpecte pomocou Proxy patternu, aby setrenie prostriedkov:
+ *      - je nutne pripajanie sa na externy system, stahovanie a parsovanie vysledkov robit pri kazdom volani?
+ *  - Podmienky:
+ *      - dbajte na dependency inversion!!!
+ */
+
 function testClient(SimpleClient $client, Url $url)
 {
-    var_dump($client->download($url));
+    var_dump(count($client->download($url)));
 }
 
 $client = new SimpleClient();
