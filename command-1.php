746060f9e78ba6c5a39516f90ed8fb071d646638
Matej Bádal <badal@profesia.sk>
2022-01-04 14:32:32 +0100

    Updated assignemnt 1

--- command-1.php
+++ command-1.php
@@ -30,20 +30,8 @@ $b = 5;
 $calculator = new Calculator();
 
 $calculator->sum($a, $b);
-printf("+ operation result: [%s] %s", $calculator->getResult(), PHP_EOL);
-
 $calculator->subtraction($a, $b);
-printf("- operation result: [%s] %s", $calculator->getResult(), PHP_EOL);
-
 $calculator->times($a, $b);
-printf("* operation result: [%s] %s", $calculator->getResult(), PHP_EOL);
-
 $calculator->division($a, $b);
-printf("/ operation result: [%s] %s", $calculator->getResult(), PHP_EOL);
-
 $calculator->power($a, $b);
-printf("^ operation result: [%s] %s", $calculator->getResult(), PHP_EOL);
-
 $calculator->square($a);
-printf("^2 operation result: [%s] %s", $calculator->getResult(), PHP_EOL);
-
