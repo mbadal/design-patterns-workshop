c9845e6e91adc817029d4ee87cbe77b03bb551c9
Matej Bádal <badal@profesia.sk>
2022-01-04 14:09:40 +0100

    Updated command-1.php instructions

--- command-1.php
+++ command-1.php
@@ -6,6 +6,24 @@ require_once 'vendor/autoload.php';
 
 use Delvesoft\Calculator\Calculator;
 
+/**
+ * Zadanie:
+ *      V balicku `Delvesoft\Calculator` je implementovana "jednoducha" implementacia kalkulacky, ktora podporuje 6 zakladny operacii: +,-,*,/,^, ^2.
+ *      Upravte kod pouzitim command patternu. Bude potrebne kazdu operaciu zabalit do vlastnej command triedy. Trieda `Delvesoft\Calculator` bude nasledne
+ *      miesto, kde sa commandy "budu spustat".
+ * Podmienky:
+ *      - Zabezpecte, aby trieda `Delvesoft\Calculator` slnala OPEN/CLOSED principle
+ *      - Riesenie implementujte do namespace `Delvesoft\DesignPattern\Command`
+ *      - AKo zaklad pre jednotlive commandy pouzitie interface `Delvesoft\DesignPattern\Command\CommandInterface`
+ * Vystup:
+ *  + operation result: [15]
+ *  - operation result: [5]
+ *  * operation result: [50]
+ *  / operation result: [2]
+ *  ^ operation result: [100000]
+ *  ^2 operation result: [100]
+ */
+
 $a = 10;
 $b = 5;
 
