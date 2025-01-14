87ac179417abfe83dd2b24497f77d6eb72d545d7
Matej Bádal <matej.badal@profesia.sk>
2025-01-14 09:34:21 +0100

    Readded missing files

---
+++ src/Sandwich/Ingredient/Vegetable/VegetableInterface.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Delvesoft\Sandwich\Ingredient\Vegetable;
+
+use Delvesoft\Sandwich\Ingredient\IngredientInterface;
+
+interface VegetableInterface extends IngredientInterface
+{
+}
