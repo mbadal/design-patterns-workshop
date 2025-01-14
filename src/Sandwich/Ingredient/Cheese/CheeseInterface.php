0ee72b3a4c9617413cdb51e336d768a6f3e5f82d
Matej Bádal <matej.badal@profesia.sk>
2025-01-14 09:37:27 +0100

    readded missing files

---
+++ src/Sandwich/Ingredient/Cheese/CheeseInterface.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Delvesoft\Sandwich\Ingredient\Cheese;
+
+use Delvesoft\Sandwich\Ingredient\IngredientInterface;
+
+interface CheeseInterface extends IngredientInterface
+{
+}
---
+++ src/Sandwich/Ingredient/Main/MainInterface.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Delvesoft\Sandwich\Ingredient\Main;
+
+use Delvesoft\Sandwich\Ingredient\IngredientInterface;
+
+interface MainInterface extends IngredientInterface
+{
+}
